89bac1c34fafa858c6d0974a42342393c35e74d7
hacking-this <108381779+hacking-this@users.noreply.github.com>
2026-03-20 17:14:27 +0530

    [643] [Time Beats: 78.50%] [Memory Beats: 72.09%] - LeetPush

--- Maximum-Average-Subarray-I.py
+++ Maximum-Average-Subarray-I.py
@@ -3,8 +3,9 @@
 3        window_sum = sum(nums[:k])
 4        max_sum = window_sum
 5        for i in range(k,len(nums)):
-6            window_sum = window_sum + nums[i]
-7            window_sum = window_sum - nums[i-k]
-8            max_sum = max(max_sum,window_sum)
-9        return max_sum/k
-10        
+6            # window_sum = window_sum + nums[i]
+7            # window_sum = window_sum - nums[i-k]
+8            window_sum += nums[i] - nums[i-k]
+9            max_sum = max(max_sum,window_sum)
+10        return max_sum/k
+11        
