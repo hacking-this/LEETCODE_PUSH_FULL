233fc6c34ad1914825f988166a00005fa2f29b54
hacking-this <108381779+hacking-this@users.noreply.github.com>
2026-03-25 23:18:31 +0530

    [1] [Time Beats: 60.61%] [Memory Beats: 53.91%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,14 +1,14 @@
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        collector = set
-4        result = []
-5        collector = {}
-6        for i, num in enumerate(nums):
-7            complement = target - num
-8
-9            if complement in collector:
-10                return [collector[complement], i]
-11            collector[num] = i
+3        hashmap = {}
+4        for i in range(len(nums)):
+5            complement = target - nums[i]
+6            if complement in hashmap:
+7                return [hashmap[complement],i]
+8            else:
+9                hashmap[nums[i]]=i
+10        
+11                
 12        
 13
 14
